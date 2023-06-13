97740a53984e9b1eb4ca3b042a0f2068b7d38d97
Anuj Sharma <anuj.sharma@qapitacorp.com>
2023-06-13 12:17:37 +0530

    project setup without cra with webpack, ts etc

---
+++ declatations.d.ts
@@ -0,0 +1,7 @@
+declare module "*.scss";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.less";
+declare module "*.pdf";
+declare module "*.module.css";
+declare module "*.module.scss";
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import styles from './index.module.scss'
+
+const App: FC = () => {
+  return <div className={styles.mainContainer}>Hello, React!</div>
+}
+
+export default App
