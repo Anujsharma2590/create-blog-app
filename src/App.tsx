49b9035a0d2b8fe6afb40dcf21b027f7a269530e
Anuj Sharma <anuj.sharma@qapitacorp.com>
2023-06-14 20:46:09 +0530

    place global css into index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 import AppRoutes from './routes/AppRoutes'
 import { BrowserRouter } from 'react-router-dom'
-import './global.css'
 
 const App: FC = () => {
   return (
