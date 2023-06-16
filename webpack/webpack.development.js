5081916cfd0e94426da21155151c4ce70abcc443
Anuj Sharma <anuj.sharma@qapitacorp.com>
2023-06-16 10:43:51 +0530

    clean webpack file and remove unused code

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -3,57 +3,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
 
-  // output: {
-  //   publicPath: "/",
-  //   path: path.resolve(__dirname, "../dist"),
-  //   filename: "[name].bundle.js",
-  // },
-
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-
-  // optimization: {
-  //   moduleIds: "deterministic",
-  //   runtimeChunk: "single",
-  //   splitChunks: {
-  //     name: false,
-  //     cacheGroups: {
-  //       vendor: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name: "vendors",
-  //         chunks: "all",
-  //       },
-  //     },
-  //   },
-  // },
-
-  // cache: {
-  //   type: "filesystem",
-  //   maxAge: 604800000,
-  //   cacheDirectory: path.resolve(__dirname, "../.temp_cache"),
-  // },
-
-  // devtool: "source-map",
-
-  //   devServer: {
-  //     historyApiFallback: true,
-  //     hot: true,
-  //     host: 'localhost',
-  //     port: 4200,
-  //     compress: true,
-  //     open: {
-  //       // target: 'https://qapmatch-open.qapitacorp.local',
-  //       target: 'https://localhost:4200',
-  //       app: {
-  //         name: 'google-chrome'
-  //       }
-  //     },
-  //     server: 'https',
-  //     allowedHosts: ['qapmatch.qapitacorp.local']
-  //   }
-  // };
 
   devServer: {
     historyApiFallback: true,
--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -8,22 +8,4 @@ module.exports = {
     path: path.resolve(__dirname, "../dist"),
     publicPath: "/",
   },
-
-  // optimization: {
-  //   moduleIds: 'deterministic',
-  //   runtimeChunk: 'single',
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     maxInitialRequests: Infinity,
-  //     minSize: 0
-  //     // name: false,
-  //     // cacheGroups: {
-  //     //   vendor: {
-  //     //     test: /[\\/]node_modules[\\/]/,
-  //     //     name: 'vendors',
-  //     //     chunks: 'all'
-  //     //   }
-  //     // }
-  //   }
-  // }
 };
